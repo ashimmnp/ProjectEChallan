60c9079e74fda3c8a88c40832c8771e95a295456
AshimNepal <np03cs4a220208@heraldcollege.edu.np>
2024-04-03 08:12:29 +0545

    Added database migration and backend for new user form

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, request, render_template, redirect, url_for
-
+from flask import Flask, request, render_template, redirect, url_for, flash
+from models import *
+import secrets
+from sqlalchemy import text
+import pymysql
+from database import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/dataechallan'
+secret_key = secrets.token_urlsafe(32)
+app.secret_key = secret_key
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/')
 def welcome():
     return render_template('index.html')
+
+
 @app.route('/add-record', methods=['POST'])
 def add_record():
     # Redirect to the page where record can be added
@@ -17,26 +31,66 @@ def add_record_page():
     # Render the template for adding a record
     return render_template('newChallan.html')
 
+
 @app.route('/adminDB')
 def adminDB():
     return render_template('adminDB.html')
 
+
 @app.route('/trafficOfficerDB')
 def trafficOfficerDB():
     return render_template('trafficOfficerDB.html')
+
 
 @app.route('/sample_login', methods=['GET', 'POST'])
 def sample_login():
     return render_template('trafficOfficerDB.html')
 
 
-@app.route('/addUser',methods=['GET', 'POST'])
+@app.route('/addUser', methods=['GET', 'POST'])
 def addUser():
+    if request.method == 'POST':
+        name = request.form['name']
+        officer_id = request.form['officer_id']
+        user_id = request.form['user_id']
+        user_name = request.form['user_name']
+        badge_number = request.form['badge_number']
+        rank = request.form['rank']
+        user_type = request.form['user_type']
+        location_assigned = request.form['location_assigned']
+        password = 'pass1234'
+
+        # Adding required record in officer table
+        new_user_officer = Officer(officerId=officer_id, username=user_name, name=name, badgeNumber=badge_number,
+                                   rank=rank, assignedLocation=location_assigned)
+
+        # Adding required records in user table
+        new_user_users = Users(userid=user_id, username=user_name, password=password, name=name, usertype=user_type)
+
+        db.session.add(new_user_officer)
+        db.session.add(new_user_users)
+
+        try:
+            db.session.commit()
+            flash('Successfully added', 'success')
+            return redirect(url_for('addUser'))
+        except:
+            db.session.rollback()
+            flash('Something went wrong')
+            return redirect(url_for('addUser'))
     return render_template('addUser.html')
+
+
+# @app.route('/')
+# def adduserSuccess():
+#     success_flag = request.args.get('adduserSuccess', False)
+#     return render_template('addUser.html', success=success_flag)
+
 
 @app.route('/newChallan', methods=['GET', 'POST'])
 def newChallan():
     return render_template('newChallan.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,67 @@
+from database import db
+
+
+class ChallanHistory(db.Model):
+    __tablename__ = 'challanhistory'
+
+    challannumber = db.Column(db.String(255), primary_key=True)
+    registrationNumber = db.Column(db.String(255), db.ForeignKey('vehicleowner.registrationNumber'))
+    violationReason = db.Column(db.String(255))
+    violationId = db.Column(db.String(255))
+    chargedAmount = db.Column(db.Numeric(10, 2))
+    dateIssued = db.Column(db.Date)
+    issuedLocation = db.Column(db.String(255))
+
+class Officer(db.Model):
+    __tablename__ = 'officer'
+
+    officerId = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    badgeNumber = db.Column(db.String(255))
+    rank = db.Column(db.String(255))
+    assignedLocation = db.Column(db.String(255))
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    usertype = db.Column(db.String(50), nullable=False)
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
+
+    vehicleId = db.Column(db.String(255), primary_key=True)
+    RegistrationId = db.Column(db.Integer)
+    RegistrationNumber = db.Column(db.String(255))
+    registrationdate = db.Column(db.Date)
+    vehicleType = db.Column(db.String(255))
+    vehicleMake = db.Column(db.String(255))
+    VehicleModel = db.Column(db.String(255))
+    ChasisNumber = db.Column(db.String(255))
+    EngineNumber = db.Column(db.String(255))
+    VehicleCategory = db.Column(db.String(255))
+    VehicleCustomtaxId = db.Column(db.Integer)
+
+class VehicleOwner(db.Model):
+    __tablename__ = 'vehicleowner'
+
+    registrationNumber = db.Column(db.String(255), primary_key=True)
+    vehicleid = db.Column(db.String(255))
+    citizenId = db.Column(db.String(255))
+    Name = db.Column(db.String(255))
+    RegistrationExp = db.Column(db.Date)
+    RegistrationStatus = db.Column(db.String(50))
+
+
+class rulesAndRegulations(db.Model):
+    __tablename__ = 'rulesAndRegulations'
+
+    rulesId = db.Column(db.String(255), primary_key=True)
+    rulecategory = db.Column(db.String(255))
+    ruleDesc = db.Column(db.String(225))
+    fineAmount = db.column(db.String(255))
+
