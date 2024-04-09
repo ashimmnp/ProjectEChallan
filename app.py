738c8e59c69c661bcf6db22ab0d033ec7dfe80ae
AadarshBIKRAM <np03cs4a220503@heraldcollege.edu.np>
2024-04-09 09:53:33 +0545

    empty commit

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-import secrets
-
 from flask import Flask, request, render_template, redirect, url_for, flash, abort, session
 from flask_login import UserMixin, login_user
-from sqlalchemy.exc import SQLAlchemyError
-
 from models import *
+import secrets
 from sqlalchemy import text
 import pymysql
 from database import db
 from flask_migrate import Migrate
 from werkzeug.security import check_password_hash
-from flask import Flask, request, render_template, redirect, url_for
-
 
 app = Flask(__name__,template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/dataechallan'
@@ -21,11 +16,12 @@ app.secret_key = secret_key
 db.init_app(app)
 migrate = Migrate(app, db)
 
-app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return render_template('index.html')
+
+
 @app.route('/add-record', methods=['POST'])
 def add_record():
     # Redirect to the page where record can be added
@@ -37,9 +33,11 @@ def add_record_page():
     # Render the template for adding a record
     return render_template('newChallan.html')
 
+
 @app.route('/adminDB')
 def adminDB():
     return render_template('adminDB.html')
+
 
 @app.route('/trafficOfficerDB')
 def trafficOfficerDB():
@@ -63,7 +61,7 @@ def login():
         password = request.form['password']
 
         # Query the database to find the user
-        user = Users.query.filter_by(username=username).first()
+        user = Users.query.filter_by(username = username).first()
 
         if user is not None and user.password == password:
             # Store user information in the session
@@ -88,13 +86,37 @@ def login():
     return render_template('index.html')
 
 
-@app.route('/sample_login', methods=['GET', 'POST'])
-def sample_login():
-    return render_template('trafficOfficerDB.html')
-
-
-@app.route('/addUser',methods=['GET', 'POST'])
+@app.route('/addUser', methods=['GET', 'POST'])
 def addUser():
+    if request.method == 'POST':
+        name = request.form['name']
+        officer_id = request.form['officer_id']
+        user_id = request.form['user_id']
+        user_name = request.form['user_name']
+        badge_number = request.form['badge_number']
+        rank = request.form['rank']
+        user_type = request.form['user_type']
+        location_assigned = request.form['location_assigned']
+        password = 'pass1234'
+
+        # Adding required record in officer table
+        new_user_officer = Officer(officerId=officer_id, username=user_name, name=name, badgeNumber=badge_number,
+                                   rank=rank, assignedLocation=location_assigned)
+
+        # Adding required records in user table
+        new_user_users = Users(userid=user_id, username=user_name, password=password, name=name, usertype=user_type)
+
+        db.session.add(new_user_officer)
+        db.session.add(new_user_users)
+
+        try:
+            db.session.commit()
+            flash('Successfully added', 'success')
+            return redirect(url_for('addUser'))
+        except:
+            db.session.rollback()
+            flash('Something went wrong')
+            return redirect(url_for('addUser'))
     return render_template('addUser.html')
 
 
@@ -102,51 +124,6 @@ def addUser():
 def newChallan():
     return render_template('newChallan.html')
 
-
-@app.route('/userManagementPortal', methods=['GET','POST'])
-def userManagementPortal():
-    # officers = Officer.query.all()
-    if request.method == 'POST':
-        search = request.form['query']
-        officers = db.session.query(Officer, Users.usertype).join(Users, Officer.username == Users.username).filter(Users.name.ilike(f'%{search}')).all()
-        return render_template('userManagementPortal.html', officers=officers)
-    else:
-        officers = db.session.query(Officer, Users.usertype).join(Users, Officer.username == Users.username).all()
-        return render_template('userManagementPortal.html', officers=officers)
-
-
-@app.route('/updateUser/<int:officer_id>', methods=['GET', 'POST'])
-def updateUser(officer_id):
-    officer = Officer.query.get_or_404(officer_id)
-    if request.method == 'POST':
-        try:
-            officer.name = request.form['name']
-            officer.badgeNumber = request.form['badge_number']
-            officer.rank = request.form['rank']
-            officer.assignedLocation = request.form['assigned_location']
-            db.session.commit()
-            flash('Successfully updated', 'success')
-            return redirect(url_for('updateUser', officer_id=officer_id))
-        except SQLAlchemyError as e:
-            db.session.rollback()
-            flash('Something went wrong in the system. Please try again', 'error')
-    return render_template('updateUser.html', officer=officer)
-
-
-@app.route('/deleteUser/<string:username>', methods=['DELETE'])
-def deleteUser(username):
-    try:
-        officer = Users.query.filter_by(username=username).first()
-
-        if officer:
-            db.session.delete(officer)
-            db.session.commit()
-            return 'Officer Deleted Successfully', 200
-        else:
-            return 'Officer Not found', 404
-    except SQLAlchemyError as e:
-        db.session.rollback()
-    return 'Problem, 500'
 
 if __name__ == '__main__':
     app.run(debug=True)
